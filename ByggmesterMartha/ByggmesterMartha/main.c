ccf1f45c714c0822fdc6be3a5042158d44be38f6
VemundRogne <vemundrogne@gmail.com>
2020-08-26 15:46:10 +0200

    exercise 1 finished.

--- ByggmesterMartha/ByggmesterMartha/main.c
+++ ByggmesterMartha/ByggmesterMartha/main.c
@@ -6,6 +6,8 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
 
 #define F_CPU	4915200
 #define BAUD	9600
@@ -22,6 +24,9 @@ void init_UART(){
 
 	// Set frame format: 8data, 2stop bit	
 	UCSR0C = (1<<URSEL0) | (1<<USBS0) | (3<<UCSZ00);
+	
+	// Enable RX interrupt
+	UCSR0B |= (1<<RXCIE0);
 }
 
 // receives one byte in polling mode
@@ -42,23 +47,40 @@ void UART_tx_polling(uint8_t data){
 	UDR0 = data;
 }
 
+ISR(USART0_RXC_vect){
+	cli();
+	//uint8_t n = UART_rx_polling();
+	
+	for (uint8_t i=0; i<5; i++){
+		PORTA |= (1<<PA0);
+		_delay_ms(200);
+		PORTA &= ~(1<<PA0);
+		_delay_ms(500);
+	}
+	sei();
+}
+
 int main(void)
 {
 	init_UART();
 	
 	DDRA |= (1<<PA0);
+
+	fdevopen(&UART_tx_polling, &UART_rx_polling);	
+//	fdevopen(&UART_tx_polling(char, stdout *), &UART_rx_polling(stdin *));
+	
+	sei();
+	
+	uint8_t number = 0;
 	
     while (1) 
     {
-		uint8_t n = UART_rx_polling();
-		UART_tx_polling(n);
-		
-		for (uint8_t i=0; i<n; i++){
-				PORTA |= (1<<PA0);
-				_delay_ms(200);
-				PORTA &= ~(1<<PA0);
-				_delay_ms(500);
-		}
+		printf("Hello number %u\r\n", number);
+		PORTA |= (1<<PA0);
+		_delay_ms(400);
+		PORTA &= ~(1<<PA0);
+		_delay_ms(1000);
+		number++;
     }
 }
 
