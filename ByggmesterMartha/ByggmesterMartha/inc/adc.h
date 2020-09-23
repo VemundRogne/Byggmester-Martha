35212ef92bc5e54590e92155eb3edc43a89b722b
VemundRogne <vemundrogne@gmail.com>
2020-09-23 14:35:44 +0200

    ADC is now internally buffered and filtered automatically
    Previously we called rd_adc whenever we wanted a reading from the adc,
    the main problem with this is that it's unfiltered.
    
    This commit adds a method of filtering the adc by using timer0 as an
    interrupt source. ISR(timer0) calls rd_adc repeatedly which has been
    modified to update an internal buffer through a simple filter.
    
    All code that previously used rd_adc must now use adc_get_values().
    This function is a drop-in-replacement to rd_adc.

--- ByggmesterMartha/ByggmesterMartha/inc/adc.h
+++ ByggmesterMartha/ByggmesterMartha/inc/adc.h
@@ -2,7 +2,7 @@
  * adc.h
  *
  * Created: 09/09/2020 09:57:51
- *  Author: vemun
+ *  Author: 
  */ 
 
 
@@ -10,6 +10,11 @@
 #define ADC_H_
 
 #include <stdio.h>
+
+// See: https://www.mikrocontroller.net/attachment/139580/schift-filter-tp.pdf
+// for info about the filter
+volatile uint16_t _adc_values[4];
+#define FILTER_CONSTANT 1
 
 // Configuration registers
 #define ADC_A0		0x00
@@ -27,14 +32,15 @@
 #define ADC_BUSY_PORT	PORTE
 #define ADC_BUSY_DDR	DDRE
 
-// NOTE: The adc is dependent on TIMER1.
-
+// NOTE: The ADC is dependent on TIMER1.
 // Sets up Timer1 to be a PWM output on PD5 (OC1A)
 void init_adc();
 
 void load_mux_config();
 
-void rd_adc(uint8_t *adc_val);
+void rd_adc();
+
+void adc_get_values(uint8_t *dest);
 
 
 #endif /* ADC_H_ */
--- ByggmesterMartha/ByggmesterMartha/src/adc.c
+++ ByggmesterMartha/ByggmesterMartha/src/adc.c
@@ -14,6 +14,7 @@
 #include <util/delay.h>
 
 // Sets up Timer1 to be a PWM output on PD5 (OC1A)
+// This is the required clock that the ADC needs to operate
 void init_adc(){
 	init_timer1();
 }
@@ -21,8 +22,6 @@ void init_adc(){
 // Loads configuration of each channel according to figura 4a (page 11) in datasheet
 void load_mux_config(){
 	volatile char *ext_adc = (char *) ADC_START_ADDRESS; // Start address for the ADC
-	
-	printf("Loading mux config \n");
 	
 	uint8_t config_data;
 	
@@ -32,17 +31,14 @@ void load_mux_config(){
 		
 		// Inhibits conversion while loading config.
 		config_data |= (1<<ADC_INH);
-		
-		// For debugging:
-		printf("  channel %u: %u\n", i, config_data);
+
 		ext_adc[i] = config_data;
 	}
 }
 
-void rd_adc(uint8_t* adc_val){
+// Updates the internally buffered adc_val register with new values from the ADC
+void rd_adc(){
 	volatile char *ext_adc = (char *) ADC_START_ADDRESS; // Start address for the ADC
-	
-	//printf("Reading values from ADC\n");
 	
 	//Write start to start conversion
 	ext_adc[0] = 0x00;
@@ -53,8 +49,12 @@ void rd_adc(uint8_t* adc_val){
 	}
 	
 	for(uint8_t i=0; i<4; i++){
-		adc_val[i] = ext_adc[i];
-		//printf("  channel %u: %u\n", i, adc_val[i]);
+		_adc_values[i] = _adc_values[i] - (_adc_values[i] >> FILTER_CONSTANT) + ext_adc[i];
 	}
 }
 
+void adc_get_values(uint8_t *dest){
+	for(uint8_t i=0; i<4; i++){
+		dest[i] = _adc_values[i] >> FILTER_CONSTANT;
+	}
+}
