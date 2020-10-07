d10f4ac79695c84335ecdd72eb4d02ec39e7dd09
martha-h <halvorsenmartha@gmail.com>
2020-10-07 17:01:04 +0200

    Added tests in test_sram

--- test/test_sram.py
+++ test/test_sram.py
@@ -13,10 +13,20 @@ def ser():
     yield ser
     ser.close()
 
+def test_sram_write(ser):
+    random_val = random.randint(0, 255)
+    assert sram_cmd.sram_write(ser, 1, random_val) == 0
+    
+
+def test_sram_read(ser):
+    assert sram_cmd.sram_read(ser, 1) != -1
+
 
 def test_sram(ser):
     random_val = random.randint(0, 255)
     sram_cmd.sram_write(ser, 1, random_val)
     read_val = sram_cmd.sram_read(ser, 1)
     assert read_val == random_val
+
+
 
