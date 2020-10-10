d67f309ad18c1a43cc38f166389f2fdcdc4b71c9
oveggeland <oskargv@stud.ntnu.no>
2020-10-10 13:37:48 +0200

    retry

--- test/test_mcp2515.py
+++ test/test_mcp2515.py
@@ -19,6 +19,12 @@ def test_mcp2515_init(ser):
     cmd.mcp2515_init(ser, mode_id)
 
     canctrl_adr = 0x0F
-    assert cmd.mcp2525_read(ser, canctrl_adr, 1) >> 5 == mode_id
+    assert cmd.mcp2515_read(ser, canctrl_adr, 1) >> 5 == mode_id
 
 
+def test_mcp2515_loopback(ser):
+    mode_id = 0b010    #Init to loopback mode
+    cmd.mcp2515_init(ser, mode_id)
+
+    test_data = [1, 1, 2, 3, 5, 8]
+    #cmd.mcp2525_write(ser, )
