58c067cb53d5500455417d0262e1c99f19efbb86
VemundRogne <vemundrogne@gmail.com>
2020-10-10 11:50:56 +0200

    add Node2 Atmel-studio project. Main turns Led1 on

---
+++ Node2/Node2/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Node2.cpp
+ *
+ * Created: 10/10/2020 11:21:35
+ * Author : vemun
+ */ 
+
+
+#include "sam.h"
+
+// PA19
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+	
+	REG_PIOA_OER |= (1<<19);
+
+    /* Replace with your application code */
+    while (1)
+    {
+		REG_PIOA_SODR |= (1<<19);
+    }
+}
