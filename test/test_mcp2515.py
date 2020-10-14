90ef1734dc8201ba3b9f321fb8036bc83d1d40f4
VemundRogne <vemundrogne@gmail.com>
2020-10-14 12:57:21 +0200

    add test that CNF-registers (node 1) are written correctly

--- test/test_mcp2515.py
+++ test/test_mcp2515.py
@@ -20,3 +20,20 @@ def test_mcp2515_init_normal(ser):
 
     canctrl_adr = 0x0F
     assert cmd.mcp2515_read(ser, canctrl_adr, 1) >> 5 == mode_id
+
+    # I've copied these expected values from the MCP_configure_bit_timing
+    # function. This verifies that the init actually managed to write
+    # the values as it tries to.
+    Expected_CNF1 = (0x3F) | (0x00)
+    Expected_CNF2 = (0x01) | (0x06 << 3)
+    Expected_CNF3 = (0x05)
+    CNF1_addr = 0x2A
+    CNF2_addr = 0x29
+    CNF3_addr = 0x28
+    read_CNF1 = cmd.mcp2515_read(ser, CNF1_addr, 1)
+    read_CNF2 = cmd.mcp2515_read(ser, CNF2_addr, 1)
+    read_CNF3 = cmd.mcp2515_read(ser, CNF3_addr, 1)
+
+    assert read_CNF1 == Expected_CNF1
+    assert read_CNF2 == Expected_CNF2
+    assert read_CNF3 == Expected_CNF3
