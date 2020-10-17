03dfc3f465f35b3f7fd2003cdd33e853b0017684
martha-h <halvorsenmartha@gmail.com>
2020-10-17 13:15:47 +0200

    Added function for transmitting js pos over CAN

--- ByggmesterMartha/ByggmesterMartha/inc/funkyboard.h
+++ ByggmesterMartha/ByggmesterMartha/inc/funkyboard.h
@@ -9,6 +9,8 @@
 #ifndef MMI_H_
 #define MMI_H_
 
+#include <avr/io.h>
+
 int joystick_offset_x;
 int joystick_offset_y;
 
@@ -32,6 +34,8 @@ enum Joystick_dir {LEFT = 0, RIGHT = 1, UP = 2, DOWN = 3, NEUTRAL = 4};
 enum Joystick_dir get_joystick_dir();
 
 struct Slider_pos get_slider_pos();
+
+uint8_t Joystick_can();
 
 //joystick_press(); return true if pressed or something 
 
