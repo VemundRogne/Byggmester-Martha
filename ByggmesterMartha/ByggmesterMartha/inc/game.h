e99d5bec3581b970b81a3c181e9a332422fe13e1
oveggeland <oveggeland@gmail.com>
2020-11-14 13:38:51 +0100

    clean up in game

--- ByggmesterMartha/ByggmesterMartha/inc/game.h
+++ ByggmesterMartha/ByggmesterMartha/inc/game.h
@@ -5,14 +5,17 @@
 #include "menu.h"
 #include "statemachine.h"
 
+// Game status variables
 uint16_t score_count;
 uint8_t game_over;
 uint8_t ir_status;
 
 void game_init();
 
+// Updates game over status based on input from IR on node 2
 void game_over_check(uint8_t ir_status);
 
+// Runs entire game-sequence, coninously called from main()
 void game_sequence();
 
 
--- ByggmesterMartha/ByggmesterMartha/src/game.c
+++ ByggmesterMartha/ByggmesterMartha/src/game.c
@@ -15,6 +15,7 @@ void game_init(){
 void game_over_check(uint8_t ir_status){
 	if (ir_status == 1){
 		game_over = 1;
+		// Enter new state in statemachine
 		enter_game_over(score_count);
 	}
 }
@@ -23,17 +24,19 @@ void game_over_check(uint8_t ir_status){
 void game_sequence(){
 	if (current_state == PLAY_GAME)
 	{
+		// Send joystick and button status to node 2
 		struct Joystick_pos js_pos_current = get_joystick_pos();
-		//struct Slider_pos slider_pos_current = get_slider_pos();
 		send_joystick_position(js_pos_current);
-		//slider_can(slider_pos_current);
 		send_button_press();
+
+		// Update score
 		score_count += 1;
 	}
 	
+	// Reset score when going back to home
 	if(current_state == HOME_MENU){
 		score_count = 0;
 	}
-		
+	
 	statemachine_execute_current_state();
 }
